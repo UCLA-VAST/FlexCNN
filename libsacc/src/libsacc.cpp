c606c1feb89a0da3a40f09cad3a66b3e45fcd662
atefehsz <atefehsz@cs.ucla.edu>
2020-08-23 14:57:24 -0700

    updated load_inst

--- libsacc/src/libsacc.cpp
+++ libsacc/src/libsacc.cpp
@@ -520,11 +520,15 @@ inline void Sacc::load_inst(
   std::ifstream in_file(input_file_path.c_str());
 
   // model configuration
+#ifdef VGG_LAYERS
   config[0] = VGG_LAYERS;
   config[1] = MobileNetV2_LAYERS;
   config[2] = STAGE1_LAYERS;
   config[3] = STAGE2_LAYERS;
   config[4] = STAGE2_ITER;
+#else
+  config[0] = LAYER_NUM;
+#endif
 
 if (in_file.is_open()){
     for (int layer_id = 0; layer_id < LAYER_NUM; layer_id++){
